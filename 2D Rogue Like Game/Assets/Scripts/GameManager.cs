cb2289669433975e644dcc8dc57e10145e45497a
lordsteggu <daniel.allison1990@gmail.com>
2017-01-15 13:34:22 +0000

    loads of stuff

--- 2D Rogue Like Game/Assets/Scripts/GameManager.cs
+++ 2D Rogue Like Game/Assets/Scripts/GameManager.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+	public float levelStartDelay = 2f;
+	public float turnDelay = 0.1f;
 	public static GameManager instance = null;
 	private BoardManager boardScript;
+	public int playerFoodPoints = 100;
+
+	[HideInInspector] public bool playersTurn = true;
 
 	private int level = 3;
+	private List<Enemy> enemies;
+	private bool enemiesMoving;
+
 
 	void Awake (){
 		if (instance == null)
@@ -16,16 +24,51 @@ public class GameManager : MonoBehaviour {
 			Destroy(gameObject);
 			
 		DontDestroyOnLoad (gameObject);
+		//Assign enemies to a new List of Enemy objects.
+		enemies = new List<Enemy>();
+
 		boardScript = GetComponent<BoardManager> ();
 		InitGame ();
 	}
 
 	void InitGame(){
+		enemies.Clear ();
 		boardScript.SetupScene (level);
+	}
+
+	public void GameOver(){
+		enabled = false;
 	}
 
 	// Update is called once per frame
 	void Update () {
-		
+		if (playersTurn || enemiesMoving)
+			return;
+
+		StartCoroutine (MoveEnemies());
+
+	}
+
+	public void AddEnemyToList(Enemy script){
+		enemies.Add (script);
+
+	}
+
+	IEnumerator MoveEnemies(){
+		enemiesMoving = true;
+		yield return new WaitForSeconds (turnDelay);
+
+		if (enemies.Count == 0) {
+			yield return new WaitForSeconds (turnDelay);
+		}
+
+		for (int i = 0; i < enemies.Count; i++) {
+			enemies [i].MoveEnemy ();
+			yield return new WaitForSeconds (enemies [i].moveTime);
+		}
+
+		playersTurn = true;
+		enemiesMoving = false;
+
 	}
 }
