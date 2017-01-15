aa9af473106c1e583d93588a506f98b1478f0d57
lordsteggu <daniel.allison1990@gmail.com>
2017-01-15 10:58:57 +0000

    Game manager created with loader

--- 2D Rogue Like Game/Assets/Scripts/BoardManager.cs
+++ 2D Rogue Like Game/Assets/Scripts/BoardManager.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
-using System.Collections.Generic;
 using Random = UnityEngine.Random;
 
 public class BoardManager : MonoBehaviour {
